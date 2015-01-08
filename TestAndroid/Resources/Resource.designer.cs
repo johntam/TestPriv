bb3b555a9a07542194ab8baf4622983451d4cb1e
John Tam, CFA <jt@machten.eu>
2015-01-08 15:26:47 +0000

    secret label added

--- TestAndroid/Resources/Resource.designer.cs
+++ TestAndroid/Resources/Resource.designer.cs
@@ -63,6 +63,9 @@ namespace TestAndroid
 			// aapt resource value: 0x7f050000
 			public const int myButton = 2131034112;
 			
+			// aapt resource value: 0x7f050001
+			public const int textView1 = 2131034113;
+			
 			static Id()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
